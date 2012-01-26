3501ac3a90a559eab40c515eea8a512ec47b60a0
Roman Greshny <greshny@gmail.com>
2012-01-26 22:29:28 +0200

    added base class and google-docs class

--- lib/hamporter/googledocs.rb
+++ lib/hamporter/googledocs.rb
@@ -1,16 +1,27 @@
 module Exporter
   class GoogleDocs < Base
-    #session = GoogleSpreadsheet.login(config['google-docs']['login'], config['google-docs']['password'])
+    def initialize
+      # FIXME: add configuration
+      @session = GoogleSpreadsheet.login(config['google-docs']['login'], config['google-docs']['password'])
+      add_worksheet
+    end
 
-    #ws = session.spreadsheet_by_key(config['google-docs']['document-key']).worksheets[0]
+    def push
+      facts = Fact.filter(:start_time => Date.parse('2011-11-01')..Date.parse('2011-11-30'))
+      fields = ['date', 'begin_time', 'finish_time', 'task', 'project']
+      facts.each_with_index do |fact, i|
+        fields.each_with_index do |field, index|
+          @ws[i + config['START_ROW'], index + 1] = fact.send field
+        end
+        @ws.save
+      end
+    end
 
-    #facts = Fact.filter(:start_time => Date.parse('2011-11-01')..Date.parse('2011-11-30'))
-    #fields = ['date', 'begin_time', 'finish_time', 'task', 'project']
-    #facts.each_with_index do |fact, i|
-      #fields.each_with_index do |field, index|
-        #ws[i + config['START_ROW'], index + 1] = fact.send field
-      #end
-      #ws.save
-    #end
+    private
+    def add_worksheet
+      @ws = session.spreadsheet_by_key(config['google-docs']['document-key']).worksheets[0]
+    end
+
+
   end
 end
