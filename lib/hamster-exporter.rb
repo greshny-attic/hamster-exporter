b26a400d40e6cb58eb3fbc79808c7f4f5ef9b7ca
Roman Greshny <greshny@gmail.com>
2012-01-26 21:59:13 +0200

    hide unworking code

--- lib/hamster-exporter.rb
+++ lib/hamster-exporter.rb
@@ -1,25 +1,26 @@
 require 'rubygems'
-require 'sequel'
-require 'google_spreadsheet'
-require 'yaml'
-require 'hamporter/application'
-require File.join(File.dirname(__FILE__), *%w[hamster activity])
-require File.join(File.dirname(__FILE__), *%w[hamster category])
-require File.join(File.dirname(__FILE__), *%w[hamster fact])
+require 'hamposter/application'
+#require 'sequel'
+#require 'google_spreadsheet'
+#require 'yaml'
+#require 'hamporter/application'
+#require File.join(File.dirname(__FILE__), *%w[hamster activity])
+#require File.join(File.dirname(__FILE__), *%w[hamster category])
+#require File.join(File.dirname(__FILE__), *%w[hamster fact])
 
-config = YAML::load(File.open("~/.hamster"))
+#config = YAML::load(File.open("~/.hamster"))
 
-DB = Sequel.connect("sqlite://#{ENV['HOME']}/.local/share/hamster-applet/hamster.db")
+#DB = Sequel.connect("sqlite://#{ENV['HOME']}/.local/share/hamster-applet/hamster.db")
 
-session = GoogleSpreadsheet.login(config['google-docs']['login'], config['google-docs']['password'])
+#session = GoogleSpreadsheet.login(config['google-docs']['login'], config['google-docs']['password'])
 
-ws = session.spreadsheet_by_key(config['google-docs']['document-key']).worksheets[0]
+#ws = session.spreadsheet_by_key(config['google-docs']['document-key']).worksheets[0]
 
-facts = Fact.filter(:start_time => Date.parse('2011-11-01')..Date.parse('2011-11-30'))
-fields = ['date', 'begin_time', 'finish_time', 'task', 'project']
-facts.each_with_index do |fact, i|
-  fields.each_with_index do |field, index|
-    ws[i + config['START_ROW'], index + 1] = fact.send field
-  end
-  ws.save
-end
+#facts = Fact.filter(:start_time => Date.parse('2011-11-01')..Date.parse('2011-11-30'))
+#fields = ['date', 'begin_time', 'finish_time', 'task', 'project']
+#facts.each_with_index do |fact, i|
+  #fields.each_with_index do |field, index|
+    #ws[i + config['START_ROW'], index + 1] = fact.send field
+  #end
+  #ws.save
+#end
